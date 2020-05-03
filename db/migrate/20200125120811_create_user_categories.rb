f9873b7680d6120caab340e2537096c9237a3aee
uko-h <hourglassu@yahoo.co.jp>
2020-05-03 16:53:26 +0900

    categoryテーブルの削除

--- db/migrate/20200125120811_create_user_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :user_categories do |t|
-      t.references :category, foreign_key: true
-      t.references :user, foreign_key: true
-      t.timestamps
-    end
-  end
-end
